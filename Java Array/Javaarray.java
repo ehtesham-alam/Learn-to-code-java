471f2a3bd043f81e4924aa1534aa2d16c4c31f5f
Ehtesham Alam <90465695+ehteshamalam74@users.noreply.github.com>
2023-01-05 20:26:33 +0530

    first commit

---
+++ Java Array/Javaarray.java
@@ -0,0 +1,20 @@
+class Javaarray {
+    public static void main(String[] args) {
+     
+      // create an array
+      int[] age = {12, 4, 5, 2, 5};
+        
+//       Accessing elements using for loop
+      for (int i = 0 ; i < age.length ; i++){
+            System.out.println("Element on index " + i + " is " + age[i]);
+      }
+   
+      // access each array elements
+//       System.out.println("Accessing Elements of Array:");
+//       System.out.println("First Element: " + age[0]);
+//       System.out.println("Second Element: " + age[1]);
+//       System.out.println("Third Element: " + age[2]);
+//       System.out.println("Fourth Element: " + age[3]);
+//       System.out.println("Fifth Element: " + age[4]);
+    }
+   }
